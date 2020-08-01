4732993abd3f1e18125d9428f6b0f688592c1fec
David Everett <davideverett1989@gmail.com>
2020-08-01 17:35:41 -0500

    Did some more practicing.

--- music.session.sql
+++ music.session.sql
@@ -100,3 +100,77 @@ ON Song.GenreId = Genre.GenreId
 GROUP BY Genre.Label;
 
 SELECT Count () as "Total Songs" FROM Song;
+
+SELECT Artist.ArtistName as "Artist", Genre.Label as "Genre"
+FROM Artist
+JOIN Album
+ON Album.ArtistId = Artist.ArtistId
+JOIN Genre
+ON Genre.GenreId = Album.GenreId
+WHERE Genre.Label = "Rock"
+GROUP BY Artist;
+
+INSERT INTO Artist (ArtistName, YearEstablished)
+VALUES ("Metallica", "1982");
+
+INSERT INTO Album (Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId)
+SELECT "The Black Album", "1991", 6240, "Elektra", Artist.ArtistId, Genre.GenreId
+FROM Artist, Genre
+WHERE Artist.ArtistName = "Metallica"
+AND Genre.Label = "Rock";
+
+INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId)
+SELECT "Enter Sandman", 532, "06/16/1991", Genre.GenreId, Artist.ArtistId, Album.AlbumId
+FROM Genre, Artist, Album
+WHERE Genre.Label = "Rock"
+AND Artist.ArtistName = "Metallica"
+AND Album.Title = "The Black Album";
+
+INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId)
+SELECT "Nothing Else Matters", 637, "06/30/1991", Genre.GenreId, Artist.ArtistId, Album.AlbumId
+FROM Genre, Artist, Album
+WHERE Genre.Label = "Rock"
+AND Artist.ArtistName = "Metallica"
+AND Album.Title = "The Black Album";
+
+SELECT Song.Title AS "Song Title", Album.Title, Artist.ArtistName AS "Artist", Album.ReleaseDate as "Release Date", Genre.Label as "Genre"
+FROM Song
+JOIN Album
+ON Song.AlbumId = Album.AlbumId
+JOIN Artist
+ON Song.ArtistId = Artist.ArtistId
+JOIN Genre
+ON Song.GenreId = Genre.GenreId
+WHERE Album.Title = "The Black Album"
+AND Artist.ArtistName = "Metallica";
+
+DELETE FROM Song
+WHERE ArtistId = 29
+AND AlbumId = 1;
+
+DELETE FROM Song
+WHERE SongId = 24;
+
+DELETE FROM Song
+WHERE SongId = 25;
+
+DELETE FROM Song
+WHERE SongId = 27;
+
+DELETE FROM Song
+WHERE SongId = 23;
+
+DELETE FROM Song
+WHERE SongId = 26;
+
+SELECT Song.Title, Artist.ArtistName as "Artist"
+FROM Song
+JOIN Artist
+ON Song.ArtistId = Artist.ArtistId
+WHERE ArtistName = "Jay Z";
+
+SELECT COUNT() AS "Number of Songs", Artist.ArtistName AS "Artist"
+FROM Song
+JOIN Artist
+ON Song.ArtistId = Artist.ArtistId
+GROUP BY Artist.ArtistName;
